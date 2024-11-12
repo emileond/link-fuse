6a2bf67a4897a273d5a174eeee16bfcec4a882cf
Emilio Sanchez <emilio@192.168.1.12>
2024-11-12 17:32:21 -0600

    bg update lp

--- src/components/marketing/Hero.jsx
+++ src/components/marketing/Hero.jsx
@@ -2,7 +2,7 @@ import { Button } from '@nextui-org/button'
 
 export default function Hero() {
   return (
-    <div className="relative justify-center items-center py-32">
+    <div className="bg-gradient-to-b from-primary-50/50 to-secondary-50/25 relative justify-center items-center py-32">
       <section className="max-w-screen-xl mx-auto px-4 gap-12 md:px-8 flex flex-col justify-center items-center">
         <span className="w-fit h-full text-sm bg-card px-2 py-1 border border-border rounded-full">
           NextUI template its here!
